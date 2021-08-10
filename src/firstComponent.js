2696e4f86119413ce36b975faf883d764b6fe08a
unknown <saransh.tyagi@soprasteria.com>
2021-08-10 10:13:36 +0530

    Bookstore application

---
+++ src/firstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render } from 'react-dom';
+
+function FirstComponent() {
+    return (
+        <react.Fragment>
+            <div>
+                <h2>This is my first component</h2>
+            </div>
+        </react.Fragment>
+    )
+}
+export default FirstComponent
